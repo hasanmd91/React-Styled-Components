b9684d9187702be1922d0f0f839111473fec7b4e
hasanmd91 <hasanmd1991@hotmail.com>
2022-06-13 19:28:04 +0300

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./assets/css/global.css";
+import Logo from './components/Logo';
+
+const App = () => {
+  return (
+    <div className='App'> 
+      <h1> learn with me   </h1>
+      <Logo />
+       </div>
+  )
+}
+
+export default App
